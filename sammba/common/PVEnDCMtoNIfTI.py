9657b54d4732a8848bf417f0fb162393a8b326ee
Nachiket NADKARNI <nn241023@finn.intra.cea.fr>
2017-08-18 15:52:06 +0200

    yet more updates

--- sammba/common/PVEnDCMtoNIfTI.py
+++ sammba/common/PVEnDCMtoNIfTI.py
@@ -1,22 +1,32 @@
 # -*- coding: utf-8 -*-
 
-#convert Bruker Paravision enhanced (multiframe) DICOM files to NIfTI-1 format
-#recursively searches a given folder for EnIm*.dcm files and saves *.nii to
-#a(nother) given folder. names the .nii based on several tags found in the .dcm
-#for FAIR_EPI also extracts TIs and saves as a .txt with same name as the .nii;
-#will eventually do the same for some other sequences (such as T1/T2 mapping)
-#only tested on PV6 .dcm and a limited number of sequences. little or no
-#error-checking. depends on dcmdump, see http://dcmtk.org/dcmtk.php.en
+#convert Bruker Paravision enhanced multiframe DICOM files into the NIfTI-1
+#format.
+
+#recursively searches a given folder for EnIm*.dcm files. for each EnIm*.dcm,
+#saves a .nii (named using several .dcm tag values) to a(nother) given folder.
+
+#for .dcm files of the FAIR_EPI sequence, also extracts TIs and saves them as a
+#.txt with same name as the .nii. will eventually do the same for some other
+#sequences (such as T1/T2 mapping).
+
+#only tested on PV6 .dcm files and a limited number of sequences. little or no
+#error-checking.
+
+#depends on the presence of a compiled version of dcmdump (part of OFFIS dcmtk;
+#http://dcmtk.org/dcmtk.php.en and http://support.dcmtk.org/docs/dcmdump.html)
+#which does the initial parsing: extraction of the header/metadata as text and
+#the image data as a raw vector
+
+#library uses:
+#numpy: affine and image matrix manipulation
+#os: standardise file/folder paths, file deletion
+#subprocess: interface dcmdump
+#itertools and pandas: image frame and parameter sorting
+#nibabel: NIfTI-1 format
+#sys: grab shell flags
 
 #%%
-
-#numpy is used for general image matrix and affine transform management
-#os handles file/folder paths and file deletion
-#subprocess interfaces to dcmdump
-#itertools and pandas generate and manage parameter tables used to sort image
-#frames from the dicom file into a "correct" order
-#nibabel creates the NIfTI-1 file
-#sys grabs shell flags to the python script
 
 import numpy as np
 import os
@@ -25,6 +35,8 @@ import itertools
 import pandas
 import nibabel
 import sys
+
+#%%
 
 def rotate_affine(angle, axis):
     """
@@ -58,8 +70,7 @@ def EnDCM_to_NII(dcmdump_path, EnDCM, save_directory, SIAPfix, valstart, splt1, 
     """
     the actual converter
     assumes int16; this will be changed in the future.
-    dcmdump_path is the path to the compiled dcmdump (part of OFFIS dcmtk),
-    which does the parsing and (later on) extraction of the image matrix.
+    dcmdump_path is the path to the compiled dcmdump
     EnDCM is the DICOM file (typically named EnIm1.dcm).
     save_directory is what it says it is
     SIAPfix is a yes or no to swapping the superior-inferior and
@@ -73,13 +84,18 @@ def EnDCM_to_NII(dcmdump_path, EnDCM, save_directory, SIAPfix, valstart, splt1, 
     of course, file/folder separators differ according to os, with unix being /
     and windows \\.
     """
+#%%
 
     #regularise/standardise input paths
     input_paths = [dcmdump_path, EnDCM, save_directory]
     input_paths = [os.path.abspath(input_path) for input_path in input_paths]
     dcmdump_path, EnDCM, save_directory = input_paths
 
-    #parse EnDCM using dcmdump
+#%%
+
+    #read EnDCM header/metadata using dcmdump
+    #+L ensures long tags are fully printed
+    #+P searches for a specific tag
     fields = subprocess.check_output([dcmdump_path, EnDCM, '+L',
         '+P', '0008,0021',   # SeriesDate
         '+P', '0008,0031',   # SeriesTime
@@ -107,6 +123,10 @@ def EnDCM_to_NII(dcmdump_path, EnDCM, save_directory, SIAPfix, valstart, splt1, 
     #(0028,0102)  # HighBit
     #(0028,0103)  # PixelRepresentation
 
+#%%
+
+    #fields that are likely to be empty or multiple are awkward to dynamically
+    #declare, so do it here rather than in the parser loop below
     TR = []  # is not always in DICOM
     protocol = []  # cos of DKI
     bruker_sequence = []  # cos of DKI
@@ -116,12 +136,26 @@ def EnDCM_to_NII(dcmdump_path, EnDCM, save_directory, SIAPfix, valstart, splt1, 
     IPPs = []
     ISPnums = []
     
+    #via subprocess, dcmdump produces numerous lines of string output.
+    #for some parameters such as SeriesDate, there should hopefully be only one
+    #line. for others, such as InStackPositionNumber, there may be several.
+    #three example lines are shown below:
+    
+    #'(0008,0021) DA [20160219]                               #   8, 1 SeriesDate'
+    #'(0018,9079) FD 35                                       #   8, 1 InversionTimes'
+    #'(0020,0032) DS [-9\\-7.417465618\\1.68366613]             #  26, 3 ImagePositionPatient'
+    
+    #fields interpreted as strings are delimited with square brackets
+    #(incidentally, several numeric fields get interpreted as strings, I do not
+    #know why, and I assume it does not matter). in the loop below, each line
+    #is parsed to identify what field it represents and extract the value
+
+#%%
+
     for line in fields.splitlines():
     
-        #dcmdump output is in bytes, easier to parse strings
         #I am guessing that a consequence of the DICOM spec and/or dcmdump mean 
         #that the field value ALWAYS starts at the same position (valstart)
-        line = str(line)
         valsfieldsep = '#  '
         splitline = line[valstart:].split(valsfieldsep)
         if len(splitline) != 2:
@@ -180,27 +214,73 @@ def EnDCM_to_NII(dcmdump_path, EnDCM, save_directory, SIAPfix, valstart, splt1, 
             cols = int(vals)
         if 'PixelSpacing' in field:  #                                0028,0030
             pixspac = [float(val) for val in vals]
+
+#%%
     
-    #InStackPositionNumber (ISPnum) is not necessarily needed. sorted unique 
-    #slice positions (used for calculating interslice gap and the affine matrix,
-    #plus doing any potential data reordering) can be determined from the slice 
-    #position information itself. however, that sounds like too much hard work
+    #next a table of certain frame parameters is generated that can be sorted
+    #by pandas. once correctly ordered, the vector describing the original
+    #positions within the DICOM image matrix is later used to extract and
+    #reorder the frames correctly
     
-    uIPPs = []  # u for unique
-    slices = max(ISPnums)
-    #I think this handles the (unlikely) possibility of InStackPositionNumber
-    #not being ordered
-    for ISPnum in range(1, slices + 1):
-        sliceindex = ISPnums.index(ISPnum)
-        uIPPs.append(IPPs[sliceindex])
-    uIPPs = np.array(uIPPs)  # find an append method to avoid this line
+    #if they have length zero (or just unequal to ISPnums, though no idea how
+    #that could be possible), populate vectors that will be included in ptbl
+    #(parameter table). there must be a more efficient way
+    if len(TIs) != len(ISPnums):
+        TIs = list(itertools.repeat(0, len(ISPnums)))
+    if len(frame_comments) != len(ISPnums):
+        frame_comments = list(itertools.repeat(0, len(ISPnums)))
+    if len(repno) != len(ISPnums):
+        repno = list(itertools.repeat(0, len(ISPnums)))
+    
+    #ptbl = parameter table
+    ptbl = pandas.DataFrame(data = {
+        'TI': TIs, 'FC': frame_comments, 'slice': ISPnums, 'slicepos': IPPs,
+        'repno': repno
+                                    }
+                            )
+    ptbl['FC'] = ptbl['FC'].astype('category')
+
+#%%
+    
+    if bruker_sequence == 'Bruker:FAIR_EPI':
+        ptbl['FC'].cat.reorder_categories(
+            ['Selective Inversion', 'Non-selective Inversion'], inplace = True)
+
+#%%
+    
+    ptbl = ptbl.sort_values(list(['repno', 'TI', 'FC', 'slice']))
+
+#%%
+    slices = max(ISPnums) #maybe a bit dangerous
+    
+    #use check_output rather than call to avoid stdout filling up the terminal
+    rawfile = os.path.join(save_directory, 'EnIm1.dcm.0.raw')
+    if os.path.exists(rawfile):
+        os.remove(rawfile)
+    sink = subprocess.check_output([dcmdump_path, EnDCM, '+W', save_directory])
+    rawarray = np.fromfile(rawfile, dtype = np.int16)
+    os.remove(rawfile)
+    
+    #rawarray is actually just a vector, need to reshape into a run of frames
+    rawarray = np.reshape(rawarray, (frames, rows, cols))
+    rawarray = rawarray[ptbl.index.values] # reorder frames using table
+    #make into 4D file. some acquisitions (such as simultaneous T1 and T2
+    #mapping) are >4D, and the NIFTI-1 standard can handle up to 7?, but I do
+    #not know any visualization software that can, so stick to 4D with a
+    #sensible use of temporal order for supplementary dimensions
+    rawarray = np.reshape(rawarray, (int(frames / slices), slices, rows, cols))
+    rawarray = np.transpose(rawarray, (3,2,1,0))
+
+
+#%% #fsp = first slice position, lsp = last slice position
+    fsp = np.array(ptbl.slicepos[ptbl.slice == 1].tolist()[1])
+    lsp = np.array(ptbl.slicepos[ptbl.slice == slices].tolist()[1])
     
     #https://en.wikipedia.org/wiki/Euclidean_distance
     #not used yet
-    IPPsdiffs = uIPPs[:-1] - uIPPs[1:]
-    eucliddists = (IPPsdiffs[:,0]**2 + IPPsdiffs[:,1]**2 + IPPsdiffs[:,2]**2)**0.5
-    if slices > 1:
-        slicegap = sum(eucliddists) / len(eucliddists)
+    flspdiff = fsp - lsp
+    eucliddist = (flspdiff[0]**2 + flspdiff[1]**2 + flspdiff[2]**2)**0.5
+    slicegap = eucliddist / (slices -1)
     
     #Documentation
     #C.7.6.2.1.1
@@ -208,26 +288,23 @@ def EnDCM_to_NII(dcmdump_path, EnDCM, save_directory, SIAPfix, valstart, splt1, 
     #common.py of dicom2nifiti by Arne Brys, icometrix
     #http://dicom2nifti.readthedocs.io
     
-    if len(uIPPs) == 1:  # single slice
+    if slices == 1:  # single slice
         step = [0, 0, -1]
     else:
-        step = (uIPPs[0] - uIPPs[-1]) / (1 - len(uIPPs))
-    #if, for some evil reason, the slices were not stored in order, I think the
-    #above line would fail. the solution would be to use InStackPositionNumber,
-    #or being even more careful, calculating which are the two outer slices
+        step = (fsp - lsp) / (1 - slices)
     
     affine = np.matrix(
-    [[-cosines[0]*pixspac[1], -cosines[3]*pixspac[0], -step[0], -uIPPs[0,0]],
-     [-cosines[1]*pixspac[1], -cosines[4]*pixspac[0], -step[1], -uIPPs[0,1]],
-     [ cosines[2]*pixspac[1],  cosines[5]*pixspac[0],  step[2],  uIPPs[0,2]],
-     [                     0,                      0,        0,           1]])
+    [[-cosines[0]*pixspac[1], -cosines[3]*pixspac[0], -step[0], -fsp[0]],
+     [-cosines[1]*pixspac[1], -cosines[4]*pixspac[0], -step[1], -fsp[1]],
+     [ cosines[2]*pixspac[1],  cosines[5]*pixspac[0],  step[2],  fsp[2]],
+     [                     0,                      0,        0,      1]])
     
     affineident = np.matrix(
     [[1, 0, 0, 0],
      [0, 1, 0, 0],
      [0, 0, 1, 0],
      [0, 0, 0, 1]])
-	
+    
     if patpos == 'HFS':
         ppaff = rotate_affine(180, 'y')
     #not sure this is correct: a reflection may be necessary too
@@ -241,47 +318,8 @@ def EnDCM_to_NII(dcmdump_path, EnDCM, save_directory, SIAPfix, valstart, splt1, 
     else:
         if SIAPfix != 'no':
             print('SIAPfix is neither yes nor no, assuming no')
-    
-	#next a table of certain frame parameters is generated that can be sorted
-	#by pandas. once correctly ordered, the vector describing the original
-	#positions within the DICOM image matrix is later used to extract and
-	#reorder the frames correctly
-	
-    #there must be a more efficient way
-    if len(TIs) != len(ISPnums):
-        TIs = list(itertools.repeat(0, len(ISPnums)))
-    if len(frame_comments) != len(ISPnums):
-        frame_comments = list(itertools.repeat(0, len(ISPnums)))
-    if len(repno) != len(ISPnums):
-        repno = list(itertools.repeat(0, len(ISPnums)))
-    
-    paramtable = pandas.DataFrame(
-        data = {'TI': TIs, 'FC': frame_comments, 'slice': ISPnums, 'repno': repno})
-    paramtable['FC'] = paramtable['FC'].astype('category')
-    
-    if bruker_sequence == 'Bruker:FAIR_EPI':
-        paramtable['FC'].cat.reorder_categories(
-            ['Selective Inversion', 'Non-selective Inversion'], inplace=True)
-    
-    paramtable = paramtable.sort_values(list(['repno', 'TI', 'FC', 'slice']))
-    
-    #use check_output rather than call to avoid stdout filling up the terminal
-    rawfile = os.path.join(save_directory, 'EnIm1.dcm.0.raw')
-    if os.path.exists(rawfile):
-        os.remove(rawfile)
-    sink = subprocess.check_output([dcmdump_path, EnDCM, '+W', save_directory])
-    rawarray = np.fromfile(rawfile, dtype = np.int16)
-    os.remove(rawfile)
-    
-	#rawarray is actually just a vector, need to reshape into a run of frames
-    rawarray = np.reshape(rawarray, (frames, rows, cols))
-    rawarray = rawarray[paramtable.index.values] # reorder frames using table
-	#make into 4D file. some acquisitions (such as simultaneous T1 and T2
-	#mapping) are >4D, and the NIFTI-1 standard can handle up to 7?, but I do
-	#not know any visualization software that can, so stick to 4D with a
-	#sensible use of temporal order for supplementary dimensions
-    rawarray = np.reshape(rawarray, (int(frames / slices), slices, rows, cols))
-    rawarray = np.transpose(rawarray, (3,2,1,0))
+
+#%%
     
     header = nibabel.Nifti1Header()
     header.set_xyzt_units('mm', 'msec')
@@ -309,7 +347,7 @@ def EnDCM_to_NII(dcmdump_path, EnDCM, save_directory, SIAPfix, valstart, splt1, 
     #need to find a better way to extract TIs
     if bruker_sequence == 'Bruker:FAIR_EPI': 
         f = open(os.path.join(save_directory, NIIname[:-7] + '_TIs.txt'), 'w')        
-        f.write(str(paramtable.TI[paramtable.slice == 1][paramtable.FC == 'Selective Inversion'].tolist())[1:-1].replace(' ',''))
+        f.write(str(ptbl.TI[ptbl.slice == 1][ptbl.FC == 'Selective Inversion'].tolist())[1:-1].replace(' ',''))
         f.close()
 
 #%%
@@ -329,6 +367,8 @@ if os.name == 'posix':
     valstart = 15
     splt1 = '\\'
     splt2 = '/'
+
+#%%
 
 for root, dirs, files in os.walk(sessdir):
     for file in files:
