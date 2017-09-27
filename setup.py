8a8496325e7b858788967e306f21862825b8d853
Salma BOUGACHA <salmabougacha@hotmail.com>
2017-09-27 15:24:20 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -1,32 +1,3 @@
-import os
-from setuptools import setup
-
-
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
-setup(
-    name="sammba",
-    version="alpha",
-    maintainer="Nachiket Nadkarni",
-    maintainer_email="nadkarni@fastmail.fm",
-    description=("MRI Rodents processing in python."),
-    license="CeCILL-B",
-    keywords="rodents registration dicom",
-    url="https://sammba-mri.github.io",
-    packages=['sammba', ],
-    long_description=read('README.rst'),
-    classifiers=[
-        "Topic :: Scientific/Engineering",
-        "License :: CeCILL-B",
-    ],
-)
-
 #! /usr/bin/env python
 
 descr = """A set of Python modules for functional MRI..."""
