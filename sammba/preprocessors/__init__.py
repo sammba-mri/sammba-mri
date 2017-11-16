5be17befbd39f1e952b0f3cb9da5b2b30ed6f48c
Salma BOUGACHA <salmabougacha@hotmail.com>
2017-11-16 17:13:09 +0100

    do not ask for cm_file

--- sammba/preprocessors/__init__.py
+++ sammba/preprocessors/__init__.py
@@ -1,3 +1,6 @@
-from utils import correct_affines, fix_obliquity
+from utils import correct_affines, fix_obliquity, create_pipeline_graph
 from func import register_func_to_anat
 from t1 import register_to_common
+
+__all__ = ['correct_affines', 'fix_obliquity', 'create_pipeline_graph',
+           'register_func_to_anat', 'register_to_common']
