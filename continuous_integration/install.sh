eadad20f612c7ff671c2e492dd86d3c608c0a915
salma1601 <salmabougacha@hotmail.com>
2017-11-18 23:23:09 +0100

    try fix builtins by future

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -96,7 +96,7 @@ if [[ "$DISTRIB" == "neurodebian" ]]; then
     bash <(wget -q -O- http://neuro.debian.net/_files/neurodebian-travis.sh)
     sudo apt-get install -qq python-scipy python-nose python-nibabel\
          python-sklearn python-pandas python-nilearn python-patsy\
-         python-networkx python-configparser
+         python-networkx python-configparser python-future
 
 elif [[ "$DISTRIB" == "conda" ]]; then
     create_new_conda_env
