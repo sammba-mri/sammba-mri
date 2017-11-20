8eddbaa6e4c2b986c5cb2d5c6b55bf01bf68edde
salma1601 <salmabougacha@hotmail.com>
2017-11-20 09:49:29 +0100

    add click and fix preprocessors imports

--- sammba/preprocessors/__init__.py
+++ sammba/preprocessors/__init__.py
@@ -1,6 +1,6 @@
-from utils import fix_obliquity, create_pipeline_graph
-from func import register_func_to_anat
-from t1 import register_to_common
+from .utils import fix_obliquity, create_pipeline_graph
+from .func import register_func_to_anat
+from .t1 import register_to_common
 
 __all__ = ['fix_obliquity', 'create_pipeline_graph',
            'register_func_to_anat', 'register_to_common']
