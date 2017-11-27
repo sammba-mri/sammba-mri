5df709c956ec2d3236452ab2745004afeb0a7cd4
salma1601 <salmabougacha@hotmail.com>
2017-11-27 18:16:52 +0100

    back to master setup

--- setup.py
+++ setup.py
@@ -81,7 +81,6 @@ if __name__ == "__main__":
               'Programming Language :: Python :: 3.4',
           ],
           packages=find_packages(),
-          package_data={'sammba.testing_data': ['*.nii.gz', '*.nii'],
-			'sammba.data_fetchers.description': ['*.rst']
+          package_data={'sammba.testing_data': ['*.nii.gz', '*.nii']
                         },
           install_requires=install_requires,)
