f4557d489c2c4b6eabcd8d77ea2a656f8370f84e
salma1601 <salmabougacha@hotmail.com>
2018-03-29 23:21:11 +0200

    fix tests

--- sammba/data_fetchers/tests/test_atlas.py
+++ sammba/data_fetchers/tests/test_atlas.py
@@ -179,12 +179,10 @@ def test_fetch_atlas_lemur_mircen_2017():
 
     # Default resolution
     bunch = atlas.fetch_atlas_lemur_mircen_2017(data_dir=tst.tmpdir, verbose=0)
-    assert_equal(len(tst.mock_url_request.urls), 2)
+    assert_equal(len(tst.mock_url_request.urls), 1)
     assert_equal(bunch['t2'],
                  os.path.join(datadir, 'MIRCen_mouselemur_template.nii.gz'))
     assert_equal(bunch['maps'],
                  os.path.join(datadir, 'MIRCen_mouselemur_atlas.nii.gz'))
-    assert_equal(nibabel.load(bunch['maps']).get_data().dtype, np.dtype(int))
-    assert_equal(len(tst.mock_url_request.urls), 1)
     assert_equal(len(bunch['labels']), 3)
     assert_not_equal(bunch.description, '')
