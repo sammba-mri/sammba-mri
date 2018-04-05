c21a69ae81811f96a192ae2c321b1384f2f2ae54
Salma BOUGACHA <salmabougacha@hotmail.com>
2018-04-05 17:51:13 +0200

    debug caching

--- sammba/externals/nipype/pipeline/engine/nodes.py
+++ sammba/externals/nipype/pipeline/engine/nodes.py
@@ -321,8 +321,8 @@ class Node(EngineBase):
         if op.exists(outdir):
             logger.debug('Output dir: %s', to_str(os.listdir(outdir)))
         hash_info = self.hash_exists(updatehash=updatehash)
+        print(hash_info)
         hash_exists, hashvalue, hashfile, hashed_inputs = hash_info
-	print(hash_info)
         logger.debug(
             'updatehash=%s, overwrite=%s, always_run=%s, hash_exists=%s',
             updatehash, self.overwrite, self._interface.always_run, hash_exists)
