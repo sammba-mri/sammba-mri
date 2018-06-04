b292b20c0c2819ef81804b1cba114920ecf260d6
salma1601 <salmabougacha@hotmail.com>
2018-06-04 20:44:22 +0200

    pull

--- sammba/registration/tests/test_struct.py
+++ sammba/registration/tests/test_struct.py
@@ -8,7 +8,6 @@ from nilearn.datasets.tests import test_utils as tst
 from sammba.registration import struct
 from nilearn._utils.niimg_conversions import _check_same_fov
 from sammba.registration.utils import _check_same_obliquity
-from sammba.registration.base import compute_brain_mask
 from sammba import testing_data
 
 
@@ -16,18 +15,15 @@ from sammba import testing_data
 def test_anats_to_common():
     anat_file = os.path.join(os.path.dirname(testing_data.__file__),
                              'anat.nii.gz')
-    brain_file = compute_brain_mask(anat_file, 400, tst.tmpdir,
-                                    use_rats_tool=False)
-
     # Check error is raised if wrong registration kind
     assert_raises_regex(ValueError, "Registration kind must be one of ",
-                        struct.anats_to_common, [anat_file], [brain_file],
-                        tst.tmpdir, registration_kind='rigidd')
+                        struct.anats_to_common, [anat_file], tst.tmpdir, 400,
+                        registration_kind='rigidd')
 
     # test common space of one image is itself
     rigid = struct.anats_to_common(
-        [anat_file], [brain_file], tst.tmpdir, registration_kind='rigid',
-        verbose=0)
+        [anat_file], tst.tmpdir, 400, registration_kind='rigid', verbose=0,
+        use_rats_tool=False)
     transform = np.loadtxt(rigid.transforms[0])
     assert_array_almost_equal(transform,
                               [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0])
@@ -37,20 +33,22 @@ def test_anats_to_common():
 def test_anat_to_template():
     anat_file = os.path.join(os.path.dirname(testing_data.__file__),
                              'anat.nii.gz')
-    brain_file = compute_brain_mask(anat_file, 400, tst.tmpdir,
-                                    use_rats_tool=False)
+    brain_file = os.path.join(os.path.dirname(testing_data.__file__),
+                              'brain.nii.gz')
 
     # Check error is raised if wrong registration kind
     assert_raises_regex(ValueError, "Registration kind must be one of ",
-                        struct.anats_to_template, [anat_file], anat_file,
-                        tst.tmpdir, 400, registration_kind='rigidd')
+                        struct.anat_to_template, anat_file, brain_file,
+                        anat_file, brain_file,
+                        tst.tmpdir, registration_kind='rigidd')
 
     # test common space of one image is itself
-    register_result = struct.anats_to_template([anat_file], anat_file,
-                                               tst.tmpdir,
-                                               400, registration_kind='rigid',
-                                               use_rats_tool=False,
-                                               verbose=0)
+    register_result = struct.anat_to_template(anat_file,  brain_file,
+                                              anat_file, brain_file, anat_file,
+                                              tst.tmpdir,
+                                              registration_kind='rigid',
+                                              use_rats_tool=False,
+                                              verbose=0)
     transform = np.loadtxt(register_result.pre_transforms[0])
     assert_array_almost_equal(transform,
                               [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0], decimal=1)
