e08a7c676ee2ce28b90c6b428524c7d45d371501
Salma BOUGACHA <salmabougacha@hotmail.com>
2018-06-14 19:13:11 +0200

    fix test

--- sammba/registration/tests/test_struct.py
+++ sammba/registration/tests/test_struct.py
@@ -16,22 +16,18 @@ from sammba import testing_data
 def test_anats_to_common():
     anat_file = os.path.join(os.path.dirname(testing_data.__file__),
                              'anat.nii.gz')
-    brain_mask_file = os.path.join(os.path.dirname(testing_data.__file__),
-                                   'mask.nii.gz')
-
     # Check error is raised if wrong registration kind
     assert_raises_regex(ValueError, "Registration kind must be one of ",
-                        struct.anats_to_common, [anat_file],
-                        [brain_mask_file], tst.tmpdir,
+                        struct.anats_to_common, [anat_file], tst.tmpdir, 400,
                         registration_kind='rigidd')
 
     # test common space of one image is itself
     rigid = struct.anats_to_common(
-        [anat_file], [brain_mask_file], tst.tmpdir, registration_kind='rigid',
-        verbose=0)
+        [anat_file], tst.tmpdir, 400, registration_kind='rigid', verbose=0,
+        use_rats_tool=False)
     transform = np.loadtxt(rigid.transforms[0])
     assert_array_almost_equal(transform,
-                              [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0], decimal=1)
+                              [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0])
 
 
 @with_setup(tst.setup_tmpdata, tst.teardown_tmpdata)
