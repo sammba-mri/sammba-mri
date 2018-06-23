2e98ec5baccc46747716675b155894d5f511c817
salma1601 <salmabougacha@hotmail.com>
2018-06-24 01:46:27 +0200

    remove redanduncies

--- sammba/registration/tests/test_template_registrator.py
+++ sammba/registration/tests/test_template_registrator.py
@@ -56,8 +56,7 @@ def test_fit_transform_anat():
     
     # Same tests, with nonlinear registration
     registrator = TemplateRegistrator(template_file, 400, output_dir=tst.tmpdir,
-                                      use_rats_tool=False, verbose=False,
-                                      registration_kind='affine')
+                                      use_rats_tool=False, verbose=False)
     assert_raises_regex(
         ValueError, 'has not been fitted',
         registrator.transform_anat_like, anat_file)
@@ -91,7 +90,8 @@ def test_fit_transform_modality():
     template_img.to_filename(template_file)
 
     registrator = TemplateRegistrator(template_file, 400, output_dir=tst.tmpdir,
-                                      use_rats_tool=False, verbose=False)
+                                      use_rats_tool=False, verbose=False,
+                                      registration_kind='affine')
     registrator.fit_anat(anat_file)
 
     assert_raises_regex(
@@ -104,6 +104,54 @@ def test_fit_transform_modality():
         ValueError, 'has not been func fitted',
         registrator.transform_modality_like, func_file, 'func')
 
+    # test fit_modality with func
+    registrator.fit_modality(func_file, 'func', slice_timing=False)
+    registered_func_img = nibabel.load(registrator.registered_func_)
+    np.testing.assert_array_almost_equal(registered_func_img.affine, template_img.affine)
+    np.testing.assert_array_equal(registered_func_img.shape[:-1], template_img.shape)
+
+    # test transform_modality
+    func_img = nibabel.load(func_file)
+    func_like_img = nibabel.Nifti1Image(np.zeros(func_img.get_data().shape[:-1]),
+                                        func_img.affine)
+    func_like_file = os.path.join(tst.tmpdir, 'func_like.nii.gz')
+    func_like_img.to_filename(func_like_file)
+    transformed_file = registrator.transform_modality_like(func_like_file, 'func')
+    transformed_img = nibabel.load(transformed_file)
+    assert_true(_check_same_fov(transformed_img, template_img))
+
+    # test transform then inverse transform brings back to the original image
+    inverse_transformed_file = registrator.inverse_transform_towards_modality(
+        transformed_file, 'func')
+    assert_true(_check_same_fov(nibabel.load(inverse_transformed_file), func_img))
+    np.testing.assert_array_equal(inverse_transformed_img.get_data(),
+                                  func_like_img.get_data())
+
+    # test inverse transform then transform brings back to the original image 
+    transformed_file2 = registrator.transform_modality_like(inverse_transformed_file, 'func')
+    assert_true(_check_same_fov(nibabel.load(transformed_file2), transformed_img))
+
+    # similar tests with perf
+    assert_raises_regex(
+        ValueError, 'has not been perf fitted',
+        registrator.transform_modality_like, func_file, 'perf')
+    m0_img = nibabel.Nifti1Image(func_img.get_data()[..., 0], func_img.affine)
+    m0_file = os.path.join(tst.tmpdir, 'm0.nii.gz')
+    m0_img.to_filename(m0_file)
+    registrator.fit_modality(m0_file, 'perf')
+    _check_same_fov(nibabel.load(registrator.registered_perf_), template_img)
+
+    # test transform_modality
+    m0_like_img = nibabel.Nifti1Image(np.zeros(m0_img.shape), m0_img.affine)
+    m0_like_file = os.path.join(tst.tmpdir, 'm0_like.nii.gz')
+    m0_like_img.to_filename(m0_like_file)
+    transformed_file = registrator.transform_modality_like(m0_like_file, 'perf')
+    _check_same_fov(nibabel.load(transformed_file), template_img)
+
+    # Similarly with nonlinear registration
+    registrator = TemplateRegistrator(template_file, 400, output_dir=tst.tmpdir,
+                                      use_rats_tool=False, verbose=False)
+    registrator.fit_anat(anat_file)
     # test fit_modality with func
     registrator.fit_modality(func_file, 'func', slice_timing=False)
     registered_func_img = nibabel.load(registrator.registered_func_)
@@ -133,14 +181,14 @@ def test_fit_transform_modality():
     transformed_img2 = nibabel.load(transformed_file2)
     np.testing.assert_array_equal(transformed_img2.affine,
                                   transformed_img.affine)
-    np.testing.assert_array_equal(transformed_img2.shape,
-                                  transformed_img.shape)
+    np.testing.assert_array_equal(transformed_img2.get_data(),
+                                  transformed_img.get_data())
 
     # similar tests with perf
     assert_raises_regex(
         ValueError, 'has not been perf fitted',
         registrator.transform_modality_like, func_file, 'perf')
-    m0_img = nibabel.Nifti1Image(func_img.get_data()[..., :-1], func_img.affine)
+    m0_img = nibabel.Nifti1Image(func_img.get_data()[..., 0], func_img.affine)
     m0_file = os.path.join(tst.tmpdir, 'm0.nii.gz')
     m0_img.to_filename(m0_file)
     registrator.fit_modality(m0_file, 'perf')
