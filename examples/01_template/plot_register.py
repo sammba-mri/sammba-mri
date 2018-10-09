2ba3ab9752887549ae65c314bf6f80cd6cd8bf94
marinacelestine <41747239+marinacelestine@users.noreply.github.com>
2018-10-09 15:51:19 +0200

    [MRG + 1] improve template creation documentation (#129)
    * improve template creation doc
    
    * improve formating template creation doc

--- examples/01_template/plot_register.py
+++ examples/01_template/plot_register.py
@@ -33,23 +33,29 @@ if not os.path.exists(write_dir):
     os.makedirs(write_dir)
 
 ##############################################################################
-# Affine registration
+# Create the template
 # -------------------
 from sammba.registration import anats_to_common
 
 affine_register = anats_to_common(retest.anat, write_dir, 400, caching=True)
 
 ##############################################################################
-# We set caching to True, so that this step computations are not restarted.
+# We set caching to True, so that the computations are not restarted.
 # The paths to the registered images can be accessed easilly
 registered_anats = affine_register.registered
 print(registered_anats)
 
 ##############################################################################
+# Assess the template
+# -------------------
+from nilearn import image
+template_img = image.mean_img(registered_anats)
+
+##############################################################################
 # Visalize results
 # ----------------
 # We plot the edges of one individual anat on top of the average image
-from nilearn import plotting, image
+from nilearn import plotting
 
 average_img = image.mean_img(registered_anats)
 display = plotting.plot_anat(average_img, dim=-1.6, title='affine register')
