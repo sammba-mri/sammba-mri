3076f3a1ea1f94dc2efc3711b17f9a876177609f
salma1601 <salmabougacha@hotmail.com>
2018-11-22 16:33:19 +0100

    specify TR for functional registration

--- examples/02_preprocessing/plot_fmri_coregistration.py
+++ examples/02_preprocessing/plot_fmri_coregistration.py
@@ -25,7 +25,7 @@ print(func_filename)
 from sammba.registration import Coregistrator
 
 coregistrator = Coregistrator(output_dir='animal_1366', brain_volume=400,
-                               use_rats_tool=False, caching=True)
+                              use_rats_tool=False, caching=True)
 print(coregistrator)
 
 ##############################################################################
@@ -42,7 +42,7 @@ print(brain_extraction_report(anat_filename, brain_volume=400,
 # Anatomical to functional registration
 # -------------------------------------
 coregistrator.fit_anat(anat_filename)
-coregistrator.fit_modality(func_filename, 'func',
+coregistrator.fit_modality(func_filename, 'func', t_r=1.,
                            prior_rigid_body_registration=True)
 
 ##############################################################################
