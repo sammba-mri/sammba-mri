7d2fdbcb7d20c2ce8cd5a3367292637ee320d446
salma1601 <salmabougacha@hotmail.com>
2018-12-05 21:32:56 +0100

    remove ipython from conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -37,8 +37,6 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
     'numpydoc.numpydoc',
-#    'IPython.sphinxext.ipython_directive',
-#    'IPython.sphinxext.ipython_console_highlighting',
     'sphinx_gallery.gen_gallery']
 
 numpydoc_show_class_members = False
