8eee3a7031f4c1b75d7c817f1241d0de5d0455d6
salma1601 <salmabougacha@hotmail.com>
2018-12-06 10:22:16 +0100

    last version nipype

--- sammba/externals/nipype/pipeline/plugins/linear.py
+++ sammba/externals/nipype/pipeline/plugins/linear.py
@@ -3,7 +3,8 @@
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 """Local serial workflow execution
 """
-from __future__ import print_function, division, unicode_literals, absolute_import
+from __future__ import (print_function, division, unicode_literals,
+                        absolute_import)
 
 import os
 
@@ -45,16 +46,15 @@ class LinearPlugin(PluginBase):
                     self._status_callback(node, 'end')
             except:
                 os.chdir(old_wd)
-                if str2bool(config['execution']['stop_on_first_crash']):
-                    raise
                 # bare except, but i really don't know where a
                 # node might fail
                 crashfile = report_crash(node)
+                if str2bool(config['execution']['stop_on_first_crash']):
+                    raise
                 # remove dependencies from queue
                 subnodes = [s for s in dfs_preorder(graph, node)]
-                notrun.append(dict(node=node,
-                                   dependents=subnodes,
-                                   crashfile=crashfile))
+                notrun.append(
+                    dict(node=node, dependents=subnodes, crashfile=crashfile))
                 donotrun.extend(subnodes)
                 if self._status_callback:
                     self._status_callback(node, 'exception')
